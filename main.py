d06704e3116c0d79c2e89eb7305f27ec4ac02084
tomluxton <ta.luxton@gmail.com>
2023-08-05 13:39:31 +1000

    Update main.py
    added a village scene with work button and energy bar. Need to connect money to each scene

--- main.py
+++ main.py
@@ -4,37 +4,108 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import random
 
 
-class SimpleGUI:
+class Scene:
     def __init__(self, root):
         self.root = root
         self.root.title("Simple GUI")
 
         # Create widgets for the main scene
-        self.label = tk.Label(root, text="Hello, GUI!")
-        self.button = tk.Button(root, text="Click Me!", command=self.on_button_click)
+        self.label = tk.Label(root, text="Walk around")
+        self.button = tk.Button(root, text="Stock Exchange", command=self.on_button_click_stock_exchange)
+        self.village_button = tk.Button(root, text="Village", command=self.on_button_click_village)
 
         # Grid layout for the main scene
         self.label.grid(row=0, column=0, padx=10, pady=10)
         self.button.grid(row=1, column=0, padx=10, pady=10)
+        self.village_button.grid(row=2, column=0, padx=10, pady=10)
 
         # Data for the line graph
         self.x_data = []
         self.y_data = []
 
-    def on_button_click(self):
+    def on_button_click_stock_exchange(self):
         # Hide the widgets of the main scene
         self.label.grid_forget()
         self.button.grid_forget()
 
         # Switch to the stock exchange scene
-        self.stock_exchange = StockExchange(self.root, self.back_to_main_scene, self.x_data, self.y_data)
+        self.stock_exchange = StockExchange(self.root, self.back_to_main_scene_from_stock_exchange, self.x_data, self.y_data)
 
-    def back_to_main_scene(self):
+    def on_button_click_village(self):
+        # Hide the widgets of the main scene
+        self.label.grid_forget()
+        self.button.grid_forget()
+
+        # Switch to the village scene
+        self.village = VillageScene(self.root, self.back_to_main_scene_from_village)
+
+    def back_to_main_scene_from_stock_exchange(self):
         # Destroy the stock exchange scene and return to the main scene
         self.stock_exchange.destroy()
         # Restore the widgets of the main scene
         self.label.grid(row=0, column=0, padx=10, pady=10)
         self.button.grid(row=1, column=0, padx=10, pady=10)
+
+    def back_to_main_scene_from_village(self):
+        # Destroy the stock exchange scene and return to the main scene
+        self.village.destroy()
+        # Restore the widgets of the main scene
+        self.label.grid(row=0, column=0, padx=10, pady=10)
+        self.button.grid(row=1, column=0, padx=10, pady=10)
+
+
+class VillageScene:
+    def __init__(self, root, on_back_button_click):
+        self.root = root
+        self.on_back_button_click = on_back_button_click
+        self.root.title("Home Scene")
+
+        # Data for the energy bar
+        self.energy_value = 100
+
+        # Create widgets for the home scene
+        self.back_button = tk.Button(self.root, text="Go Back", command=self.on_back_button_click)
+        self.money_label = tk.Label(root, text="Money: $0")
+        self.work_button = tk.Button(root, text="Work", command=self.work)
+        self.energy_label = tk.Label(root, text="Energy:")
+        self.energy_bar = tk.Canvas(root, width=200, height=20, bg='white')
+        self.update_energy_bar()
+        self.sleep_button = tk.Button(root, text="Sleep", command=self.reset_energy)
+
+        # Grid layout for the home scene
+        self.money_label.grid(row=0, column=0, padx=10, pady=10)
+        self.work_button.grid(row=1, column=0, padx=10, pady=10)
+        self.energy_label.grid(row=2, column=0, padx=10, pady=5)
+        self.energy_bar.grid(row=3, column=0, padx=10, pady=5)
+        self.sleep_button.grid(row=4, column=0, padx=10, pady=10)
+        self.back_button.grid(row=5, column=0, padx=10, pady=10)
+
+        # Data for the line graph
+        self.x_data = []
+        self.y_data = []
+
+    def update_energy_bar(self):
+        self.energy_bar.delete("energy")
+        bar_width = (self.energy_value / 100) * 200
+        self.energy_bar.create_rectangle(0, 0, bar_width, 20, fill='green', tags="energy")
+
+    def work(self):
+        self.money_label.config(text=f"Money: ${int(self.money_label['text'][8:]) + 20}")
+        self.energy_value = max(self.energy_value - 10, 0)
+        self.update_energy_bar()
+
+    def reset_energy(self):
+        self.energy_value = 100
+        self.update_energy_bar()
+
+    def destroy(self):
+        # Hide and destroy widgets of the village scene
+        self.money_label.grid_forget()
+        self.work_button.grid_forget()
+        self.energy_label.grid_forget()
+        self.energy_bar.grid_forget()
+        self.sleep_button.grid_forget()
+        self.back_button.grid_forget()
 
 
 class StockExchange:
@@ -54,8 +125,8 @@ class StockExchange:
         self.current_value_label = tk.Label(self.root, text="Current Y-Value: ")
         self.buy_button = tk.Button(self.root, text="Buy", command=self.buy_stock)
         self.sell_button = tk.Button(self.root, text="Sell", command=self.sell_stock)
-        self.stocks_label = tk.Label(self.root, text="Stocks: 0")
-        self.money_label = tk.Label(self.root, text="Money: $200")
+        self.stocks_label = tk.Label(self.root, text=f"Stocks: {self.stocks}")
+        self.money_label = tk.Label(self.root, text=f"Money: ${self.money}")
 
         # Grid layout for the stock exchange scene
         self.new_label.grid(row=0, column=0, padx=10, pady=10)
@@ -128,5 +199,5 @@ class StockExchange:
 if __name__ == "__main__":
     # Create the main application window
     root = tk.Tk()
-    app = SimpleGUI(root)
+    home_location = Scene(root)
     root.mainloop()
